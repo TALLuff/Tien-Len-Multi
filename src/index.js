482fca99a4ae2fb7dd5731ee6432b94ba9379b5d
Northcoders <northcoders@Northcoderss-MacBook-Pro.local>
2020-02-25 09:57:09 +0000

    Extract config from index

--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as firebase from "firebase/app";
-
-var firebaseConfig = {
-  apiKey: "AIzaSyCU36VI_s7BD2aCJTBCTxAomUXh_e0xOsE",
-  authDomain: "tien-len-multi.firebaseapp.com",
-  databaseURL: "https://tien-len-multi.firebaseio.com",
-  projectId: "tien-len-multi",
-  storageBucket: "",
-  messagingSenderId: "137319330002",
-  appId: "1:137319330002:web:34caa6f7891f5a67"
-};
+import { firebaseConfig } from "./config";
 
 firebase.initializeApp(firebaseConfig);
 
